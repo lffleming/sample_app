304375e6373f2f9ab50e7a70661ec497bb98be19
lffleming <lfleming@lichia-wifi.caiena.lan>
2014-07-18 11:56:40 -0300

    Add default host to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,6 @@ SampleApp::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.action_mailer.default_url_options = { :host => "secret-sea-1190.herokuapp.com" }
+
 end
